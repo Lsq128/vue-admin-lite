cde3e47bcbbf9ff2852c6c6cd3736d4c5dde0e64
Lsq128 <924500075@qq.com>
2022-02-18 00:09:56 +0800

    feat(框架): 初始化框架

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// 配置文档: https://eslint.bootcss.com/docs/user-guide/configuring
+module.exports = {
+  // 制定配置文件父级
+  root: true,
+  // 指定环境的全局变量，下面的配置指定为浏览器环境
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  // 指定eslint解析器
+  parser: "vue-eslint-parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  globals: {
+    defineProps: "readonly",
+    defineEmits: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
+  },
+};

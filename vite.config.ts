d033f0e48acce15417dcca61c5bc190a85cb9000
Liushengqun <18232366809@163.com>
2022-02-18 18:34:43 +0800

    feat: 配置windicss和app loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from "vite";
 import * as path from "node:path";
 import vue from "@vitejs/plugin-vue";
+import WindiCSS from "vite-plugin-windicss";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig((configEnv) => {
   console.log(configEnv);
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      WindiCSS(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            appName: "vue-admin-lite",
+            appTitle: "vue-admin-lite",
+          },
+        },
+      }),
+    ],
     css: {
       preprocessorOptions: {
         scss: {
